cc4e22d2874117e5a7a8f9f893a7f035616410aa
Mohammad Izan Khan <your-email@example.com>
2026-02-16 22:18:38 +0530

    fix vpc sg attach

--- TASK-6-Automate-Strapi-Deployment/terraform/main.tf
+++ TASK-6-Automate-Strapi-Deployment/terraform/main.tf
@@ -1,20 +1,25 @@
 resource "aws_instance" "strapi" {
- ami = "ami-053b0d53c279acc90"
+  ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   key_name      = "strapi-key"
 
-  security_groups = [aws_security_group.strapi_sg.name]
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+
+  associate_public_ip_address = true
 
   user_data = <<-EOF
 #!/bin/bash
 apt update -y
 apt install docker.io -y
-systemctl start docker
 systemctl enable docker
+systemctl start docker
+usermod -aG docker ubuntu
 
 docker pull ${var.docker_user}/strapi-task6:${var.image_tag}
 docker run -d -p 1337:1337 ${var.docker_user}/strapi-task6:${var.image_tag}
 EOF
 
-  tags = { Name = "Strapi-Task6" }
+  tags = {
+    Name = "Strapi-Task6"
+  }
 }
