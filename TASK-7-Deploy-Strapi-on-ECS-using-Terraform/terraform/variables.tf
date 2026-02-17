65899021d7bd907f4951a617465703e45d4a7eed
Mohammad Izan Khan <your-email@example.com>
2026-02-17 19:26:02 +0530

    Task7: Secure RDS inside private subnet

---
+++ TASK-7-Deploy-Strapi-on-ECS-using-Terraform/terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "project_name" {
+  type    = string
+  default = "strapi-task7"
+}
+
+variable "db_username" {
+  type    = string
+  default = "strapi"
+}
+
+variable "db_password" {
+  type    = string
+  default = "Strapi12345"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
