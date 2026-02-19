9b4496cfe0d607ecb43c26bfaf3e7b32e09f1984
Mohammad Izan Khan <your-email@example.com>
2026-02-19 19:57:38 +0530

    fix: properly add Task 8 files

---
+++ TASK-8-Deploy-Strapi-on-ECS-Fargate-using-Terraform-Monitoring/terraform/security.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "ecs" {
+  name   = "ecs-strapi-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
